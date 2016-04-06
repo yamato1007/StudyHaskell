48491d0f928caecd53c30ff552a2fcab0ccb0be0
yamato_tanaka <a.niger.333141592653589793@gmail.com>
2016-04-07 02:09:59 +0900

    Bird.hsを対話的なプログラムにした

--- 11_sugoiHaskell_lesson/Bird.hs
+++ 11_sugoiHaskell_lesson/Bird.hs
@@ -1,5 +1,3 @@
-module Bird where
-
 import Control.Monad
 import Control.Monad.Writer
 
@@ -24,6 +22,53 @@ addBirdLog b p = writer (p >>= (flip addBird b), [show p])
 addBirdsLog :: [(Bird,Bird)] -> Pole -> Writer [String] (Maybe Pole)
 addBirdsLog b p = foldM (flip addBirdLog) (Just p) b
 
+
+printPole :: Maybe Pole -> IO ()
+printPole p = putStrLn $ "Now Birds...\n  " ++ (show' p)
+  where show' (Just p') = show p'
+        show' Nothing = "Failed..."
+
+printHistory :: [String] -> IO ()
+printHistory s = do
+  putStrLn "History..."
+  putStr $ unlines $ map ("  "++) s
+
+printPoleHistory :: Writer [String] (Maybe Pole) -> IO ()
+printPoleHistory w = let (p, h) = runWriter w
+                     in do
+                       printHistory h
+                       printPole p
+                       putStrLn ""
+
+promptLine :: String -> IO String
+promptLine s = do
+  putStr s
+  getLine
+
+inputInteger :: String -> IO Integer
+inputInteger s = do
+  numStr <- promptLine s
+  case reads numStr of
+    [(n,"")] -> return n
+    _ -> inputInteger "Try again: "
+
+inputBird :: IO (Bird,Bird)
+inputBird = do
+  birdl <- inputInteger "How many birds is comming to left side? : "
+  birdr <- inputInteger "How many birds is comming to right side? : "
+  return (birdl,birdr)
+  
+dialogueCycle :: Writer [String] (Maybe Pole) -> IO ()
+dialogueCycle w = do
+  printPoleHistory w
+  b <- inputBird
+  putStrLn ""
+  dialogueCycle $ w >>= (addBirdLog b)
+
 main = do 
-  print $ runWriter $ addBirdsLog [(1,3),(2,1),(1,2),(3,1)] (0,0)
-  print $ runWriter $ addBirdsLog [(1,3),(2,1),(-1,2),(3,1)] (0,0)
+  --printPoleHistory $ addBirdsLog [(1,3),(2,1),(1,2),(3,1)] (0,0)
+  --printPoleHistory $ addBirdsLog [(1,3),(2,1),(-1,2),(3,1)] (0,0)
+  dialogueCycle $ return $ Just (0,0)
+
+
+
